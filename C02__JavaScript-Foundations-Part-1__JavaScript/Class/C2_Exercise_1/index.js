ca6bc033ab10cf79e1bf5da857481b05274f7f65
GeeWilleh <geewilleh@gmail.com>
2022-02-17 22:49:33 -0600

    Ignore file name change

--- C02__JavaScript-Foundations-Part-1__JavaScript/Class/C2_Exercise_1/index.js
+++
@@ -1,8 +0,0 @@
-var string = 'name';
-var number = 2;
-var boolean = true;
-var null1 = null;
-var undefined = undefined;
-var object = {'text': 2, name: null}
-var array = [William, Yaboi, Gee]
-var name;
