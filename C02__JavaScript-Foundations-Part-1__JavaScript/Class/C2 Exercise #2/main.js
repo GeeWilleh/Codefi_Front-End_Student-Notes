ca5fe380a8fa9cffb57b807fc067fabe230c1ce3
GeeWilleh <geewilleh@gmail.com>
2022-02-17 22:14:22 -0600

    Exercise 1&2 upload

---
+++ C02__JavaScript-Foundations-Part-1__JavaScript/Class/C2 Exercise #2/main.js
@@ -0,0 +1,13 @@
+const food = {
+    Cucumber : .25,
+    Ramen : .75,
+    ribEye : 49,
+};
+
+for (let i = 0; i < users.length; i++) {
+
+    '<li>' + food[0] + '</li>' + '<li>' + food[1] + '</li>' + '<li>' + food[2] + '</li>'
+
+}
+
+console.log('<ul>' + food[i] + '</ul>')
